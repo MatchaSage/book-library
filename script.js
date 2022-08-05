a06eca3feff56dd64b58943c1b8476d2bb016bbb
Treyce Wheeler <t.zeryon@gmail.com>
2022-08-04 20:21:17 -0400

    Initial commit

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
+let bookShelf = document.querySelectorAll('div.book.cards');
+
 let bookArray = [];
 
 function Books(title, author, pages, read) {
     this.title = title
-    this.author - author
+    this.author = author
     this.pages = pages
     this.read = read
-    this.info = function() {
-        return `${title} by ${author}, ${pages}, ${read}.`;
-    }
 }
 
 function storeBook(title, author, pages, read) {
@@ -17,6 +16,29 @@ function storeBook(title, author, pages, read) {
 
 function displayBooks() {
     bookArray.forEach(book =>{
-        //do something with the book.
+        let bookDiv = document.createElement('div');
+        bookDiv.classList.add('card');
+        let titleContainer = document.createElement('p');
+        titleContainer.classList.add('book', 'title');
+        let authorContainer = document.createElement('p');
+        let pagesContainer = document.createElement('p');
+        let readStatus = document.createElement('p');
+
+        titleContainer.textContent = book.title;
+        authorContainer.textContent = book.author;
+        pagesContainer.textContent = book.pages;
+        readStatus.textContent = book.read;
+
+        bookDiv.appendChild(titleContainer);
+        bookDiv.appendChild(authorContainer);
+        bookDiv.appendChild(pagesContainer);
+        bookDiv.appendChild(readStatus);
+        
+        bookShelf[0].appendChild(bookDiv)
+        console.log(bookShelf)
     })
 }
+
+storeBook('meme', 'dog', 250, 'read');
+storeBook('hella', 'kitten', 500,'unread');
+displayBooks();
