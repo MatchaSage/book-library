7af9ff06a052d80782a0125a6514ae3df7487f5a
Treyce Wheeler <t.zeryon@gmail.com>
2022-08-05 20:44:47 -0400

    Finish remove button

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 let bookShelf = document.querySelectorAll('div.book.cards');
 let bookButton = document.querySelectorAll('button.book.button');
 let bookForm = document.getElementById('add book');
+let bookRead = document.getElementById('read');
+let childArray = bookShelf[0].children;
+
+
 let bookArray = [];
+
+bookRead.addEventListener('click', () => {
+    if (bookRead.value == 'Read') {
+        bookRead.value = 'Unread';
+    }
+
+    else {
+        bookRead.value = 'Read';
+    }
+})
+
+bookForm.addEventListener("submit", function(event) {
+    event.preventDefault()
+    let bookTitle = document.getElementById('title').value;
+    let bookAuthor = document.getElementById('author').value;
+    let bookPages = document.getElementById('pages').value;
+    let bookRead = document.getElementById('read');
+    storeBook(bookTitle, bookAuthor, bookPages, bookRead.value);
+})
 
 bookButton[0].addEventListener('click', function() {
     if (bookForm.style.display != 'block') {
@@ -25,9 +48,19 @@ function Books(title, author, pages, read) {
 function storeBook(title, author, pages, read) {
     let tmpBook = new Books(title, author, pages, read);
     bookArray.push(tmpBook);
+    displayBooks();
 }
 
+function clearDisplay() {
+    while (bookShelf[0].firstChild) {
+        bookShelf[0].removeChild(bookShelf[0].firstChild);
+    }
+}
+
+
 function displayBooks() {
+    clearDisplay();
+
     bookArray.forEach(book =>{
         let bookDiv = document.createElement('div');
         bookDiv.classList.add('card');
@@ -36,29 +69,30 @@ function displayBooks() {
         let authorContainer = document.createElement('p');
         let pagesContainer = document.createElement('p');
         let readStatus = document.createElement('p');
+        let removeBook = document.createElement('button');
+        
+        for (let i = 0; i < childArray.length; i++) {
+
+        }
+
+        removeBook.textContent = 'Remove Book';
 
         titleContainer.textContent = book.title;
         authorContainer.textContent = book.author;
         pagesContainer.textContent = book.pages;
         readStatus.textContent = book.read;
-
         bookDiv.appendChild(titleContainer);
         bookDiv.appendChild(authorContainer);
         bookDiv.appendChild(pagesContainer);
         bookDiv.appendChild(readStatus);
+        bookDiv.appendChild(removeBook);
         
         bookShelf[0].appendChild(bookDiv)
+
+        removeBook.addEventListener('click', function() {
+            // this.parentElement.remove();
+            console.log(childArray.length);
+            
+        })
     })
 }
-
-storeBook('meme', 'dog', 250, 'read');
-storeBook('hella', 'kitten', 500,'unread');
-storeBook('meme', 'dog', 250, 'read');
-storeBook('hella', 'kitten', 500,'unread');
-storeBook('meme', 'dog', 250, 'read');
-storeBook('hella', 'kitten', 500,'unread');
-storeBook('meme', 'dog', 250, 'read');
-storeBook('hella', 'kitten', 500,'unread');
-storeBook('meme', 'dog', 250, 'read');
-storeBook('hella', 'kitten', 500,'unread');
-displayBooks();
